9e0281ad17b6b1b3fb3428269e698b7c45c9ca4f
Diney <dineydm@gmail.com>
2021-11-03 18:59:28 -0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Valdiney Menezes on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Valdiney Menezes on 03/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
